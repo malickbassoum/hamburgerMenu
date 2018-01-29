32851f2dd5ba3e4bab195d7cfba6a152b0f2bda9
Bassoum <mabassoum@gmail.com>
2018-01-29 10:56:01 +0000

    menuHamburger

---
+++ app/src/main/java/com/bassoum/hamburgermenu/MainActivity.java
@@ -0,0 +1,40 @@
+package com.bassoum.hamburgermenu;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawerLayout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.hambugerMenu:
+                drawerLayout.openDrawer(Gravity.RIGHT);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
